80e2b6243c32671b2df01cc3eca98246aac6ed36
tebanwolox <esteban.bedoya@wolox.co>
2019-05-24 17:55:03 -0500

    delete unnecesary import

--- ng5/src/app/screens/register/register.component.ts
+++ ng5/src/app/screens/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { User } from '../../models/user';
 import { HttpClient } from '@angular/common/http';
