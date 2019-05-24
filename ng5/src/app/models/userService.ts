03f1307be5f1e625d5e12ed65d25d3a0ac325da4
tebanwolox <esteban.bedoya@wolox.co>
2019-05-24 17:57:49 -0500

    delete empty constructor

--- ng5/src/app/models/userService.ts
+++ ng5/src/app/models/userService.ts
@@ -4,9 +4,6 @@ import { HttpClient } from '@angular/common/http';
 export class UserService {
   readonly URL_SERVICE = 'https://wbooks-api-stage.herokuapp.com';
 
-  constructor(){
-  }
-
   createUser(user: User, http:HttpClient) {
     http.post<any>(this.URL_SERVICE + '/api/v1/users', {user}).toPromise()
       .then(res => {
