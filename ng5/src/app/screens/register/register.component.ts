4fe99b9db5365b0e0c81a386b4cd1c8e5329eb1a
tebanwolox <esteban.bedoya@wolox.co>
2019-05-24 10:40:15 -0500

    change carpet structure

---
+++ ng5/src/app/screens/register/register.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { User } from './user';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent {
+
+  rForm: FormGroup;
+  post: any;
+  firstName: string = '';
+  lastName: string = '';
+  email: string = '';
+  password: string = '';
+  titleAlert: String = 'This field is required';
+  subTitle: String = 'BOOKS';
+
+  constructor(private fb: FormBuilder) {
+    this.rForm = fb.group({
+      'firstName': [null, Validators.required],
+      'lastName': [null, Validators.required],
+      'email': [null, Validators.compose([Validators.required, Validators.email])],
+      'password': [null, Validators.required],
+    });
+  }
+
+  register(post) {
+    this.firstName = post.firstName;
+    this.lastName = post.lastName;
+    this.email = post.email;
+    this.password = post.password;
+    let user: User;
+    user = {email: this.email, 
+            first_name: this.firstName, 
+            last_name: this.lastName,
+            locale: 'en',
+            password: this.password,
+            password_confirmation: this.password
+          }
+  
+    console.log({user});
+  }
+}
