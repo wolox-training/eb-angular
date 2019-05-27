391177510fb090cb45c70a37a71ea60cf42ed1e9
tebanwolox <esteban.bedoya@wolox.co>
2019-05-27 17:02:31 -0500

    add to gitignore environment files

--- ng5/src/app/models/userService.ts
+++ ng5/src/app/models/userService.ts
@@ -1,6 +1,6 @@
 import { User } from './user'; 
 import { HttpClient } from '@angular/common/http';
-import { environment } from '../../environments/environment'
+import { environment } from '../../environments/environment';
 
 export class UserService {
   
