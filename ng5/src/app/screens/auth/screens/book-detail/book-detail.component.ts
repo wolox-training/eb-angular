e48da6d814b648093950afbf37e5a7f358cc41b4
tebanwolox <esteban.bedoya@wolox.co>
2019-05-31 16:48:42 -0500

    add book-detail information

--- ng5/src/app/screens/auth/screens/book-detail/book-detail.component.ts
+++ ng5/src/app/screens/auth/screens/book-detail/book-detail.component.ts
@@ -10,6 +10,12 @@ import { BookService } from '../../../../models/book.service';
 export class BookDetailComponent implements OnInit {
 
   id: string;
+  img: string;
+  title: string;
+  publisher: string;
+  year: string;
+  genre: string;
+  author: string;
 
   constructor(
     private route: ActivatedRoute,
@@ -20,7 +26,13 @@ export class BookDetailComponent implements OnInit {
     this.id = this.route.snapshot.params.id;
     this.bookService.getBookById(this.id)
       .subscribe(res => {
-        console.log(res);
+        this.img = res.image_url;
+        this.title = res.title;
+        this.publisher = res.publisher;
+        this.year = res.year;
+        this.genre = res.genre;
+        this.author = res.author;
+        this.author = res.author;
       });
   }
 }
